8ca02a1166458e0463bf0b2328d6c18f442fd183
denghui <denghui9999@gmail.com>
2019-03-10 22:31:54 +0800

    第二次补充Vector

--- chapter2/Vector.cpp
+++ chapter2/Vector.cpp
@@ -1,23 +1,17 @@
 #include "Vector.h"
 #include <random>//置乱函数使用置乱器
-#include <utility>//使用swap()函数
+#include <utility> //使用swap()函数
+#include <iostream>
 
+
+/////////////内部接口
 template <typename T>
-void Vector<T>::copyform(T const *A, Rank lo, Rank hi) //A[lo,hi)
+void Vector<T>::copyform(const T *A, Rank lo, Rank hi) //A[lo,hi)
 {
-    _elem = new T[_CAPACITY = (lo + hi) << 1];
+    _elem = new T[_CAPACITY = (hi - lo) << 1];
     _size = 0;
     while (lo < hi)
         _elem[_size++] = A[lo++];
-}
-
-template <typename T>
-Vector<T> &Vector<T>::operator=(Vector<T> const &V)
-{
-    if (_elem)
-        delete[] _elem;
-    copyform(V.elem(), 0, V.size());
-    return *this;
 }
 
 template <typename T>
@@ -46,28 +40,165 @@ void Vector<T>::shrink()
     for (int i = 0; i != _size;++i)
         _elem[i] = oldElem[i];
     delete[] oldElem;
+    //std::cout << "容量已缩减！！！" << std::endl;  //测试使用，已验证容量已经可以缩减
 }
 
-template<typename T>
-T & Vector<T>::operator[](Rank r)const
-{
-    return *_elem[r];
-}
-
-template<typename T>
+template <typename T>
 void Vector<T>::permute()
 {
     std::default_random_engine e;
     for (int i = _size; i != 0; --i)
-        std::swap(*this[i - 1], *this[e() % i]);
+        std::swap(_elem[i - 1], _elem[e() % i]);
 }
 
 
+///////////只读接口
+template <typename T>
+Rank Vector<T>::find(const T &e, Rank lo, Rank hi) const //[lo,hi)
+{
+    while ((lo < hi--) && (e != _elem[hi]))
+        ;
+    return hi;
+}
+
+template <typename T>
+void Vector<T>::show() const
+{
+    for (int i = 0; i != _size; ++i)
+        std::cout << _elem[i] << '\t';
+}
 
 
+///////////可写接口
+template <typename T>
+Vector<T> &Vector<T>::operator=(const Vector<T> &V)
+{
+    if (_elem)
+        delete[] _elem;
+    copyform(V.elem(), 0, V.size());
+    return *this;
+}
+
+template <typename T>
+T & Vector<T>::operator[](Rank r)const
+{
+    return _elem[r];
+}
+
+template <typename T>
+void Vector<T>::unsort(Rank lo,Rank hi)     //[lo,hi)
+{
+    std::default_random_engine e;
+    T *V = _elem + lo;
+    for (int i = hi - lo; i != 0; --i)
+        std::swap(V[i - 1], V[e() % i]);
+}
+
+template <typename T>
+void Vector<T>::unsort()
+{
+    //unsort(0, _size);
+    permute();
+}
+
+template <typename T>
+Rank Vector<T>::insert(Rank r, const T &e)    //0 <= r <_size
+{
+    expand();
+    for(int i=_size;i!=r;--i)
+        _elem[i] = _elem[i - 1];
+    _elem[r] = e;
+    ++_size;
+    return r;
+}
+
+template <typename T>
+int Vector<T>::remove(Rank lo,Rank hi)
+{
+    if(lo==hi)
+        return 0;
+    while(hi<_size)
+        _elem[lo++] = _elem[hi++];
+    _size = lo;
+    shrink();
+    return hi - lo;
+}
+
+template <typename T>
+T Vector<T>::remove(Rank r)
+{
+    T e = _elem[r];
+    remove(r, r + 1);
+    return e;
+}
+
+int main()
+{
+    int a[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    Vector<int> Vec1(10, 5, 5);
+    Vector<int> Vec2(a, 8);
+    Vector<int> Vec3(a, 2, 5);
+    Vector<int> Vec4(Vec2);
+    Vector<int> Vec5(Vec2, 2, Vec2.size());
+
+    Vec1.show();
+    std::cout << std::endl;
+    Vec2.show();
+    std::cout << std::endl;
+    Vec3.show();
+    std::cout << std::endl;
+    Vec4.show();
+    std::cout << std::endl;
+    Vec5.show();
+    std::cout << std::endl;
+    //测试各个构造函数   copyfrom()
+
+    Vec3.show();
+    std::cout << std::endl;
+    Vec3 = Vec2;
+    Vec3.show();
+    std::cout << std::endl;
+    Vec3.unsort(Vec3.size() >> 1, Vec3.size());
+    Vec3.show();
+    std::cout << std::endl;
+    Vec3.unsort();
+    Vec3.show();
+    std::cout << std::endl;
+    //测试=   两个unsort() permute()
+
+    Vec3[0] = -1;
+    Vec3.show();
+    std::cout << std::endl;
+    //测试[]
+
+    std::cout << Vec3.empty() << '\t' << Vec3.size() << '\t' << *Vec3.elem() << std::endl;
+    //测试 上述三个函数
+
+    std::cout << Vec3.find(-2) << '\t' << Vec3.find(-1, 4, 8) << std::endl;
+    //测试两个find()
+
+    Vector<int> Vec6(3, 3, 1);
+    Vec6.show();
+    std::cout << Vec6.size() << std::endl;
+    Vec6.insert(1, -1);
+    Vec6.show();
+    std::cout << Vec6.size() << std::endl;
+    //测试expand()和insert();
+
+    Vector<int> Vec7(10, 10, 7);
+    Vec7.show();
+    std::cout << std::endl;
+    std::cout << Vec7.size() << std::endl;
+    std::cout << Vec7.remove(2, 10) << std::endl;
+    Vec7.show();
+    std::cout << std::endl;
+    std::cout << Vec7.size() << std::endl;
+    std::cout << Vec7.remove(0) << std::endl;
+    Vec7.show();
+    std::cout << std::endl;
+    std::cout << Vec7.size() << std::endl;
+    //测试两个 remove() 和 shrink()
 
 
-
-
-
-int main() { return 0; }
+    return 0;
+}
--- chapter2/Vector.h
+++ chapter2/Vector.h
@@ -8,12 +8,16 @@ template <typename T> //向量模板类
 class Vector
 {
   protected:
+  //数据成员
     Rank _size;    //规模
     int _CAPACITY; //容量      规模<=容量
     T *_elem;      //数据区
-    void copyform(T const *A, Rank lo, Rank hi);
-    void expand();
-    void shrink();
+  //内部接口
+    void copyform(const T *A, Rank lo, Rank hi);//复制数组A的[lo,hi)
+    void expand();//装填因子为50%
+    void shrink();//小于_size<_capacity的25%缩小
+    void permute(); //置乱器，将向量置为乱序，内部接口，对外提供unsort()
+
 
   public:
     Vector(int c = DEFAULT_CAPACITY, int s = 0, T v = 0) //容量为c，规模为s，初始化为v
@@ -22,19 +26,32 @@ class Vector
         for (_size = 0; _size < s; _elem[_size++] = v)
             ;
     }
-    Vector(T const *A, int n) { copyform(A, 0, n); }
-    Vector(T const *A, int lo, int hi) { copyform(A, lo, hi); }
-    Vector(Vector<T> const &V) { copyform(V._elem(), 0, V._size()); }
-    Vector(Vector<T> const &V, Rank lo, Rank hi) { copyform(V._elem(), lo, hi); }
-    ~Vector() { delete[] _elem; }
+    Vector(const T *A, int n) { copyform(A, 0, n); }//以数组A的前n-1的元素生成对象
+    Vector(const T *A, int lo, int hi) { copyform(A, lo, hi); }//以区间[lo,hi)的元素生成对象
+    Vector(const Vector<T> &V) { copyform(V.elem(), 0, V.size()); }//生成的对象中的元素与V相同
+    Vector(const Vector<T> &V, Rank lo, Rank hi) { copyform(V.elem(), lo, hi); }//以V中[lo,hi)的元素生成对象
+    ~Vector() { delete[] _elem; }//析构函数，释放内存
+    
+    
     //只读接口
-    T *elem() const { return _elem; }
-    Rank size() const{ return _size; }
-    bool empty() const{ return !_size; }
+    T *elem() const { return _elem; }//返回头指针
+    Rank size() const{ return _size; }//返回_size
+    bool empty() const{ return !_size; }//空向量返回1，否则返回0
+    Rank find(const T &e, Rank lo, Rank hi) const; //无序查找：在[lo,hi)之间找e
+    Rank find(const T &e) const { return find(e, 0, _size); } //无序查找：在[0,_size)之间找e
+    void show() const;//测试使用，显示向量中各个元素的值
+    
+    
+    //可写接口
+    Vector<T> &operator=(const Vector<T> &); //向量间赋值
+    T &operator[](Rank r) const;//使用[]获得元素的引用
+    void unsort(Rank lo, Rank hi);//将区间[lo,hi)置乱
+    void unsort();//整体置乱[0,_size)
+    Rank insert(Rank r, const T &e);//在r出插入e 其与之后元素后移一位,返回插入位置的秩
+    Rank insert(const T &e) { return insert(_size, e); }//在最后插入一个元素,返回最后一个元素的秩
+    int remove(Rank lo, Rank hi);//去除[lo,hi)间的元素，后面元素前移，返回去除元素的个数
+    T remove(Rank r);//去除秩为r的元素，后面元素后移，返回去除的元素的值
 
-    Vector<T> &operator=(Vector<T> const &);//
-    T &operator[](Rank r) const;
-    void permute();//置乱器，将向量置为乱序
 };
 
 
